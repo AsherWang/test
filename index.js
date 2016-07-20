f7e7d4017a1a21c152820f8d59c2506a572eb928
asher <1057488870@qq.com>
2016-07-20 10:09:11 +0800

    done!

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by asher on 16-7-19.
+ */
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtp://heavenduke%40heavenduke.com:940815cc.,@heavenduke.com');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"测试一下" <heavenduke@heavenduke.com>', // sender address
+    to: 'wangaoxin@foxmail.com', // list of receivers
+    subject: '最近下大雨了', // Subject line
+    text: '要注意身体啊孩子', // plaintext body
+    html: '<b>身体</b>要紧' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
